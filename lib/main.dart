a07b567b59e8df748565977de3be5cb4f73a263c
Kuwuamenu-Kofi-Gabriel <jkuwuamenu@gmail.com>
2023-10-24 15:12:01 +0000

    refactor: Refactor the counter class

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_try/presentation/counter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,43 +18,3 @@ void main() {
   );
 }
 
-class Counter extends StatelessWidget {
-  const Counter({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("Flutter_Bloc Counter")),
-        body: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<CounterCubit, CounterState>(
-              builder: (context, state) {
-                return Text(
-                  "The counter is: ${state.countValue}",
-                  style: const TextStyle(fontSize: 30),
-                );
-              },
-            ),
-            const SizedBox(height: 30),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                  },
-                  child: const Icon(Icons.remove),
-                ),
-                const SizedBox(width: 10),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                  },
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
-        )
-    );
-  }
-}
